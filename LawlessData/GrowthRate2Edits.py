d6097aa3353eb499156584ea562dc98fc7279317
HAHerrmann <h.a.herrmann2@newcastle.ac.uk>
2016-05-26 13:13:21 +0200

    Area must be a circle

--- LawlessData/GrowthRate2Edits.py
+++ LawlessData/GrowthRate2Edits.py
@@ -68,7 +68,7 @@ def getBlobs(image,bk,showIms=False,DX=25,DY=25):
         imnum+=1
     cv.SetImageROI(im2,(DX,DY,siz[0],siz[1]))
     #temporarily crop the image according to background
-    cv.Copy(im,im2) ##NOT SURE ABOUT THIS STEP
+    cv.Copy(im,im2) #makes a copy and renames it 
     cv.ResetImageROI(im2)
     if showIms:
         cv.SaveImage("3ShowImages%05d.png"%imnum,im2) #input image 
@@ -84,37 +84,32 @@ def getBlobs(image,bk,showIms=False,DX=25,DY=25):
         imnum+=1
 
     # Find objects and fill holes with Contour
-    storage=cv.CreateMemStorage()    #WHY?
-    element_shape = cv.CV_SHAPE_RECT #WHY? 
-    for x in xrange(1,10):
-        cv.Dilate(dst,dst) #same input and output
-        if showIms:
-            cv.SaveImage("5ShowImages%05d.png"%imnum,dst) #
-            imnum+=1
-        cv.Erode(dst,dst) ##NOT SURE ABOUT THIS STEP 
-        if showIms:
-            cv.SaveImage("6ShowImages%05d.jpg"%imnum,dst)
-            imnum+=1
+    storage=cv.CreateMemStorage()    
+    element_shape = cv.CV_SHAPE_RECT 
+    #Changed the dilation so that now it actually fills in the circles fully 
+    cv.Dilate(dst,dst,iterations=5)
+    if showIms:
+        cv.SaveImage("5ShowImages%05d.png"%imnum,dst)
+        imnum+=1
+    cv.Erode(dst,dst,iterations=5)
+    if showIms:
+       cv.SaveImage("6ShowImages%05d.png"%imnum,dst)
+       imnum+=1
     clone=cv.CloneImage(dst)
     imcol=cv.CloneImage(im2)
     contour=cv.FindContours(clone, storage, cv.CV_RETR_TREE, cv.CV_CHAIN_APPROX_SIMPLE, (0, 0))
     FCA=[]
     while contour:
-        #print(contour)
-        area=cv.ContourArea(contour) #area of blob 
-        print(area*math.pi*4) 
+        area=cv.ContourArea(contour)
+        peri=cv.ArcLength(contour)
         i,(x,y),radius=cv.MinEnclosingCircle(contour)
-        print((radius*2*math.pi)**2) #this should be roughly equal to the above if full circle
-        print(" ") #BUT WHERE TO SET THE CUT-OFF?? 
-        if area>15.0: #WHY THIS NUMBER? 
-            
-            #black=cv.CreateImage(cv.GetSize(im2),8,1)
-            #colourex=cv.CV_RGB(255,255,255)
-            #cv.DrawContours(black,contour,colourex,colourex,0,cv.CV_FILLED,8)
-            #FCA.append(black)
+        circumference=2*math.pi*radius
+        area2=math.pi*(radius**2)
+        if (0<area/area2>0.8):
             FCA.append(contour)
         contour=contour.h_next()
     return(FCA)
+
 
 def imageFromBlob(blobs,bk):
     tot=1392640
@@ -140,8 +135,6 @@ def imageFromBlob(blobs,bk):
             #cv.DrawContours(black,blob,colourex,colourex,0,cv.CV_FILLED,8)
     return(black)
 
-##Where these numbers manually/arbitrarily chosen? 
-# Final photo number...
 FINALPHOT=35
 # Size of border (px) to put around image
 DX,DY=25,25
@@ -179,13 +172,13 @@ for f in folders:
     print(f)
     # Get blobs from final photo
     finalphoto="img_%09d__000.tif"%FINALPHOT
-    blbs=getBlobs(os.path.join(fullpath,f,finalphoto),bk,DX=DX,DY=DY) #showIms=True,
+    blbs=getBlobs(os.path.join(fullpath,f,finalphoto),bk,showIms=False,DX=DX,DY=DY) #showIms=True,
     ##THIS IS THE FUNCTION TO EDIT 
 
     # Make numpy array for storing results
     NBlbs=len(blbs)
     res=numpy.zeros((FINALPHOT,NBlbs+1),numpy.float) #why plus one? because the first row will be the time 
-    locs=numpy.zeros((NBlbs,4),numpy.int)  #what the 4 columns for? 
+    locs=numpy.zeros((NBlbs,4),numpy.int)
 
     # Make directory for output images
     if not os.path.exists(os.path.join(fullpath,"OutputImages",f)):
@@ -203,31 +196,35 @@ for f in folders:
             tiffcount+=1
     for imno in xrange(0,FINALPHOT): #iterating through all image numbers less than 36
         imname=tiffs[imno]
-        print(imno)
+        #print(imno)
         imtim=os.path.getmtime(os.path.join(fullpath,f,imname)) #gets time of when image was saved? 
         res[imno,0]=imtim #stores the time 
         # Get current blobs
-        cblbs=getBlobs(os.path.join(fullpath,f,imname),bk)
-        kjhkjhljl
+        cblbs=getBlobs(os.path.join(fullpath,f,imname),bk,showIms=False)
         #gets ALL current blobs for that image number in that folder
         
         # Paint all current blobs to an empty image
         black=cv.CreateImage(cv.GetSize(bk),8,1)
         colourex=cv.CV_RGB(255,255,255) #white
+        cblbno=0
         for cblb in cblbs:
             #draws a white contour around the blob
             cv.DrawContours(black,cblb,colourex,colourex,0,cv.CV_FILLED,8)
         blbno=1
-        for blb in blbs:
-            ROI=cv.BoundingRect(blb) #cuts out the rectangle of the last blob 
-            cv.SetImageROI(black,ROI) #for background specifies where rectangle goes 
-            maskedarea=cv.CountNonZero(black) #Does this give the area!?!?!? 
-            #print maskedarea,cv.GetSize(black)
-            cv.ResetImageROI(black)
-            res[imno,blbno]=maskedarea
-            locs[blbno-1]=[ROI[0]-DX,ROI[1]-DY,ROI[2],ROI[3]] #DONT UNDERSTAND THIS BIT;
-            #must be generating final blob images 
-            blbno+=1
+        for blb in blbs: #iterating through final photos
+            #Implement check for circle at each photo here!!!!!!!!!!
+            
+            ROI=cv.BoundingRect(blb) #cuts out the rectangle of the last blob
+            cv.SetImageROI(black,ROI) #for background specifies where rectangle goes
+            maskedarea=cv.CountNonZero(black) #Area
+            #print maskedarea, blbno, f
+            if maskedarea is not 0:
+                cv.ResetImageROI(black)
+                res[imno,blbno]=maskedarea
+                locs[blbno-1]=[ROI[0]-DX,ROI[1]-DY,ROI[2],ROI[3]]
+                blbno+=1
+            else:
+                print("Not a valid growth curve ")
         blbno=0
         for blb in blbs:
             ROI=cv.BoundingRect(blb)
