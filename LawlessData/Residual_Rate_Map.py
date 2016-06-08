73219af90d6884379150f6e4b7b518a94ee070a0
HAHerrmann <h.a.herrmann2@newcastle.ac.uk>
2016-06-08 20:49:41 +0100

    Improved Output Image

--- LawlessData/Residual_Rate_Map.py
+++ LawlessData/Residual_Rate_Map.py
@@ -18,9 +18,16 @@ w,h=example.size
 col_chart1=Image.open("Colour_Chart_Res.png")
 col_chart2=Image.open("Colour_Chart_Rate.png")
 col_w,col_h=col_chart1.size
-plate=Image.new('RGB',(w*8+col_w,h*8),"grey")
+plate=Image.new('RGB',(w*8+col_w+(9*10),h*8+(8*10)),"grey")
 plate.paste(col_chart2,(0,0))
 plate.paste(col_chart1,(0,col_h))
+
+##if os.name=="posix":
+##    font=ImageFont.truetype("/usr/share/fonts/truetype/msttcorefonts/arial.ttf", 80)
+##else:
+##    font=ImageFont.truetype("arial.ttf", 80)
+
+font=ImageFont.load("arial.pil")
 
 counter=0
 for f in folders:
@@ -30,9 +37,9 @@ for f in folders:
     true_f=pinid(f)
     col=int(true_f[4:])
     ro=int(true_f[1:3])
-    plate.paste(curr_im,(w*(col-15)+col_w,h*(ro-3)))
+    plate.paste(curr_im,(w*(col-15)+(col-14)*10+col_w,h*(ro-3)+(ro-2)*10))
     draw=ImageDraw.Draw(plate)
-    draw.text((w*(col-15)+col_w,h*(ro-3)),f,(255,255,255))
+    draw.text((w*(col-15)+(col-14)*10+col_w,h*(ro-3)+(ro-2)*10),font=font)
 
 plate.show()
     
