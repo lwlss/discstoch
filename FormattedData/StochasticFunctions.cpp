aee32dfd18423ebb2a85f60353a669b951b93898
HAHerrmann <h.a.herrmann2@newcastle.ac.uk>
2016-06-16 15:20:54 +0100

    more rcpp f'ns -mcmc one need editing

--- FormattedData/StochasticFunctions.cpp
+++ FormattedData/StochasticFunctions.cpp
@@ -1,13 +1,6 @@
 #include <Rcpp.h> 
 using namespace Rcpp;
 
-/*** R
-aft = function(x,y,t){
-  af = approx(x,y,t,method="constant")
-  return(af$y)
-}
-*/
-
 // [[Rcpp::export]]
 double simDt_cpp(int K=1000, double r=1.0, int N0=1, int NSwitch=100, double t0=0, double t1=1) {
   if (NSwitch>N0){
@@ -22,15 +15,55 @@ double simDt_cpp(int K=1000, double r=1.0, int N0=1, int NSwitch=100, double t0=
     ats.push_front(t0);
     double tmax=max(ats);
     if (tmax>=t1){
-      NumericVector aft1;
-      Environment G = Rcpp::Environment::global_env();
-      Function aft = G["aft"];
-      aft1=aft(ats,clist,t1);
-      return aft1[0];
+      NumericVector which=ats[ats < t1];
+      int ind=which.length();
+      double ats_t1=(clist[ind-1]-clist[ind])/(ats[ind-1]-ats[ind])*(t1-ats[ind])+clist[ind];
+      return ats_t1;
     }else{
       return K*NSwitch*exp(r*(t1-tmax))/(K+NSwitch*(exp(r*(t1-tmax))-1));
     }
   }else{
     return K*N0*exp(r*(t1-t0))/(K+N0*(exp(r*(t1-t0))-1));
   }
+}
+
+// [[Rcpp::export]]
+List pf_cpp(Function simx0, int n, int t0, 
+            NumericVector times, NumericVector deltas, 
+            Function dataLik, Function stepFun, 
+            int i, NumericVector area){
+  NumericMatrix w(n,1);
+  NumericMatrix xmat=simx0(n,t0);
+  for (int j=0; j<n; j=j+1){
+    NumericVector xm=stepFun(xmat(j,_), times(i-1), deltas(i-1));
+    xmat(j,_)=xm;
+    NumericVector wj = dataLik(xmat(j,_),times(i),area(i-1));
+    w(j,_)=wj;
+  }
+  return List::create(_["xmat"]=xmat, _["w"]=w);
+}
+
+// [[Rcpp::export]]
+NumericMatrix mcmc_cpp(int p, double tune, int iters, int thin, Function mLLik, NumericVector th){
+  double ll=-1e99;
+  NumericMatrix thmat(iters,p);
+  for (int i=0; i<iters; i=i+1){
+    Rcout << "Iter " << i << std::endl;
+    for(int j=0; j<thin; j=j+1){
+      NumericVector thprob=th*exp(rnorm(p,0,tune));
+      NumericVector llprob=mLLik(thprob);
+      NumericVector pr=log(runif(1));
+      Rcout << "llstuff " << llprob[0]-ll << std::endl;
+      Rcout << "Prob " << pr[0] << std::endl;
+      if (pr[0]<(llprob[0]-ll)){
+        NumericVector th=thprob;
+        NumericVector ll=llprob;
+        Rcout << "thprob " << thprob << std::endl;
+        Rcout << "llprob " << llprob << std::endl;
+      }
+    }
+    Rcout << "th " << th << std::endl;
+    thmat(i,_)=th;
+  }
+  return thmat;
 }
