24966d21d266bd50c104f949e26ce2b500508cd6
HAHerrmann <hherrmann2@gmail.com>
2016-12-05 11:15:27 +0000

    Naomi Data; Image Number for Strain

---
+++ LevySiegalData/NaomiData/NaomiDataExploring.R
@@ -0,0 +1,29 @@
+# Naomi Data New
+
+setwd('./NZ_130721_1_GR')
+load('d.Rfile')
+
+labhaploid = which(d$pID[,2] == "FY4")
+strain_imno = c()
+for (i in names(labhaploid)){
+  id = which(i == names(d$well.list))
+  strain_imno = c(strain_imno,d$well.list[[id]])
+}
+
+setwd('..')
+
+setwd('./NZ_130722_1_GR')
+load('d.Rfile')
+
+strain_imno = c()
+for (i in names(labhaploid)){
+  id = which(i == names(d$well.list))
+  strain_imno = c(strain_imno,d$well.list[[id]])
+}
+
+print(length(strain_imno))
+print(length(unique(strain_imno)))
+
+setwd('..')
+# Saving image numbers associated with that strain to file 
+lapply(strain_imno, write, "FY4_ImNo.txt", append=TRUE)
